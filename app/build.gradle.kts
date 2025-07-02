4425b9bcbab6677ab67d1582f99e1d23c6d96ba2
Yurii Zinchuk <zinchukyurii25@gmail.com>
2025-07-02 20:03:51 +0300

    created App class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
@@ -38,8 +40,18 @@ android {
         compose = true
     }
 }
+kapt {
+    correctErrorTypes = true
+}
+
 
 dependencies {
+
+    // Hilt
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+
     // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/main/java/com/zinchuk/moviescatalog/App.kt
@@ -0,0 +1,7 @@
+package com.zinchuk.moviescatalog
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
